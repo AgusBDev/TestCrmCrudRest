28e10ae47d5114ae1c7b8edcb84cb03b0c2ccee8
Agustin Blanco <ablanco3500@gmail.com>
2022-02-26 13:30:30 -0300

    Commit Proyecto a GitHub

---
+++ js/API.js
@@ -0,0 +1,63 @@
+const url = 'http://localhost:4000/clientes';
+
+export const nuevoCliente = async cliente => {
+    
+    try {
+        console.log(cliente);
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify( cliente ),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+export const getClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteCliente = async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE',
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getClienteById = async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const putCliente = async cliente => {
+    try {
+        await fetch(`${url}/${cliente.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
